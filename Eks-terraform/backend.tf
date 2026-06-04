c711f2efd67cc72e624c126b69ff9a88f1de511e
Ketan Dhadve <134600326+CloudwithKetan@users.noreply.github.com>
2026-06-04 20:33:42 +0530

    Fix S3 bucket name formatting
    Removed extra space from S3 bucket name.

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "zxcvbnmplkmnoiujhuhgvfgtfdtfrd " # Replace with your actual S3 bucket name
+    bucket = "zxcvbnmplkmnoiujhuhgvfgtfdtfrd" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-north-1"
   }
