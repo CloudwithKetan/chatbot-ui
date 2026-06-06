850ffc38bf24d731a5961b82d8a1ff9f5361439d
Ketan Dhadve <134600326+CloudwithKetan@users.noreply.github.com>
2026-06-06 13:07:36 +0530

    Update S3 bucket and region in backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "zxcvbnmplkmnoiujhuhgvfgtfdtfrd" # Replace with your actual S3 bucket name
+    bucket = "balti-bucket-pune" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "eu-north-1"
+    region = "us-east-1"
   }
 }
